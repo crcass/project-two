8730533f30ab1b60be7184e00d47a8c5301a4af4
PENA5 <44989115+PENA5@users.noreply.github.com>
2019-04-01 20:55:33 -0500

    routes/apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,4 +18,28 @@ module.exports = function(app) {
       return res.json(data);
     });
   });
+
+app.get("/api/contact", (req, res) => {
+  db.Contact.findAll({}).then(data => {
+    console.log(data);
+   return res.json(data);
+  });
+
+});
+
+// Create a new example
+app.post("/api/contact", (req, res) => {
+  db.Contact.create(req.body).then(data => {
+   return res.json(data);
+  });
+});
+
+// Delete an example by id
+app.delete("/api/contact/:id", function(req, res) {
+  db.Devpool.destroy({ where: { id: req.params.id } }).then(function(
+    Devpool_db
+  ) {
+    res.json(Devpool_db);
+  });
+});
 };
